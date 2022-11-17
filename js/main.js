dd438e05cc0b8a8dc435e9e36befe19823fdb3a3
jjang051 <flyjjang051@gmail.com>
2022-11-17 12:00:23 +0900

    section02

--- js/main.js
+++ js/main.js
@@ -23,3 +23,30 @@ section01TL
 .from(".section01 .txt0302", { x: -100, opacity: 0})
 .from(".section01 .txt0303", { x: 100, opacity: 0})
 .from(".section01 .txt04", { scale: 2, opacity:0})
+
+const section02TL = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".section02",
+    markers: {
+      fontSize: "20px",
+      indent: 20,
+      startColor: "#fff",
+      endColor: "#f00",
+    },
+    start: "top top",
+    end: "bottom top",
+    scrub: 1,
+    pin: true,
+  },
+});
+
+/* prettier-ignore*/
+section02TL
+.from(".section02 .title", { backgroundColor:"transparent" })
+.from(".section02 .title .char", { x: 100, opacity: 0, stagger: { each: 0.05 } })
+.from(CSSRulePlugin.getRule(".tsa .section02 .txt01:after"),{cssRule:{scaleX:0}})
+.from(".section02 .mainTxt01", { x: 100, opacity: 0})
+.from(".section02 .mainTxt02", { x: -100, opacity: 0})
+.from(".section02 .mainTxt03", { x: 100, opacity: 0})
+.from(".section02 .circleList li", { x: 100, opacity: 0,stagger:{each:0.5}})
+.from(".section02 .cduBox", { scale: 2, opacity:0})
